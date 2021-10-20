363cb16aad8e05c306bf99c21863c3615314b754
XZ0T1Cz- <69109292+NopasitJ@users.noreply.github.com>
2021-10-20 15:58:37 +0700

    Delete Test1 directory

--- Test1/test2.py
+++
@@ -1,8 +0,0 @@
-i = int(input("Input : "))
-if i>0:
-    for kk in range(i):
-        print(kk*'*'+(i*2-kk*2)*' '+kk*'*')
-    for kk in range(i,0,-1):
-        print(kk*'*'+(i*2-kk*2)*' '+kk*'*')
-else:
-    print("!!!Please enter number greater than zero!!!")
