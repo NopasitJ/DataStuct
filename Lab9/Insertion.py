59f9771ac9e2dd24674bdd57fd72ec3e576f3b50
XZ0T1Cz- <69109292+NopasitJ@users.noreply.github.com>
2021-11-12 21:39:01 +0700

    Add files via upload

---
+++ Lab9/Insertion.py
@@ -0,0 +1,33 @@
+def insertion_index(lst, curr_index, value):  
+    if curr_index > 0 and lst[curr_index - 1] > value:  
+        lst[curr_index] = lst[curr_index - 1] 
+        return insertion_index(lst, curr_index - 1, value) 
+    else:
+        return curr_index 
+
+
+def insertion_sort(lst, start=0, length=None, progress=0):
+    if length is None:
+        length = len(lst)
+
+    value = lst[start]
+
+    index = insertion_index(lst, start, value)  
+    lst[index] = value 
+    progress += 1  
+
+    if progress > 1:
+        if len(lst[progress:]) != 0:
+            print(f"insert {value} at index {index} : {lst[:progress]} {lst[progress:]}")
+        else:
+            print(f"insert {value} at index {index} : {lst[:progress]}")
+
+    if start+1 < length:  
+        insertion_sort(lst, start+1, length, progress)  
+
+
+
+inp = list(map(int, input("Enter Input : ").split()))
+insertion_sort(inp)
+print("sorted")
+print(inp)
